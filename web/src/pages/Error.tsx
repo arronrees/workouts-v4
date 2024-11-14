9d7d5373e3c875e04bc9dddebdd37e43472675b3
arronrees <64908445+arronrees@users.noreply.github.com>
2024-11-14 07:48:34 +0000

    Update error page

--- web/src/pages/Error.tsx
+++ web/src/pages/Error.tsx
@@ -1,16 +1,45 @@
-import { useRouteError } from 'react-router-dom';
+import { isRouteErrorResponse, Link, useRouteError } from 'react-router-dom';
 
 export default function ErrorPage() {
   const error = useRouteError();
-  console.error(error);
 
-  return (
-    <div id='error-page'>
-      <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
-      <p>
-        <i>{error.statusText || error.message}</i>
-      </p>
-    </div>
-  );
+  if (isRouteErrorResponse(error)) {
+    return (
+      <main className='grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8'>
+        <div className='text-center'>
+          <p className='text-base font-semibold text-green-light'>404</p>
+          <h1 className='mt-4 text-balance text-5xl font-semibold tracking-tight text-green-dark sm:text-7xl'>
+            <i>{error.statusText}</i>
+          </h1>
+          <p className='mt-6 text-pretty text-lg font-medium text-green-mid sm:text-xl/8'>
+            Sorry, we couldn’t find the page you’re looking for.
+          </p>
+          <div className='mt-10 flex items-center justify-center gap-x-6'>
+            <Link to='/' className='btn'>
+              Go back home
+            </Link>
+          </div>
+        </div>
+      </main>
+    );
+  } else {
+    return (
+      <main className='grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8'>
+        <div className='text-center'>
+          <p className='text-base font-semibold text-green-light'>500</p>
+          <h1 className='mt-4 text-balance text-5xl font-semibold tracking-tight text-green-dark sm:text-7xl'>
+            <i>Something went wrong.</i>
+          </h1>
+          <p className='mt-6 text-pretty text-lg font-medium text-green-mid sm:text-xl/8'>
+            Sorry, we couldn’t process your request. Please try again later.
+          </p>
+          <div className='mt-10 flex items-center justify-center gap-x-6'>
+            <Link to='/' className='btn'>
+              Go back home
+            </Link>
+          </div>
+        </div>
+      </main>
+    );
+  }
 }
